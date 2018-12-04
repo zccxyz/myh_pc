6f5888b4b9251710d73da42346c98ae442ccbeef
Your Name <you@example.com>
2018-12-04 18:03:00 +0800

    'x'

--- js/func.js
+++ js/func.js
@@ -1,3 +1,25 @@
 /*
 * 公共方法文件
 * */
+//成功
+function success(msg) {
+    this.$message(msg);
+}
+//警告
+function warn(msg) {
+    this.$message({
+        message: msg,
+        type: 'success'
+    });
+}
+//消息
+function msg(msg) {
+    this.$message({
+        message: msg,
+        type: 'warning'
+    });
+}
+//错误
+function error(msg) {
+    this.$message.error(msg);
+}
