a55ccc48eca1e549a248b88293a6d573433f3de9
Your Name <you@example.com>
2018-12-07 10:00:17 +0800

    'xx'

--- js/func.js
+++ js/func.js
@@ -1,5 +1,5 @@
-//const ym = 'https://admin.myhkj.cn/apis/backend/';
-const ym = 'http://admin.myh.com/apis/backend/';
+const ym = 'https://admin.myhkj.cn/apis/backend/';
+//const ym = 'http://admin.myh.com/apis/backend/';
 
 const methods = {
     'login': 'login/login',
