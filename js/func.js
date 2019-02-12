3b20c24970083d34fe774a373bae5ddd212acce2
xyz <xyz@xyzs-iMac.local>
2019-02-12 11:34:22 +0800

    xx

--- js/func.js
+++ js/func.js
@@ -1,5 +1,5 @@
-//var ym = 'https://www.myhkj.cn/apis/backend/';
-var ym = 'http://admin.test.com/apis/backend/';
+var ym = 'https://www.myhkj.cn/apis/backend/';
+//var ym = 'http://admin.test.com/apis/backend/';
 // var ym = 'http://127.0.0.1:8000/apis/backend/';
 
 var methods = {
