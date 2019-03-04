f6c7b3a0296f857ae13e34e1f074864f57593beb
youmei <278412835@qq.com>
2019-03-04 10:28:32 +0800

    'up'

--- js/func.js
+++ js/func.js
@@ -1,6 +1,6 @@
-var ym = 'https://www.myhkj.cn/apis/backend/';
+// var ym = 'https://www.myhkj.cn/apis/backend/';
 // var ym = 'http://admin.test.com/apis/backend/';
-// var ym = 'http://127.0.0.1:8000/apis/backend/';
+var ym = 'http://127.0.0.1:8000/apis/backend/';
 
 var methods = {
     'login': 'login/login',
